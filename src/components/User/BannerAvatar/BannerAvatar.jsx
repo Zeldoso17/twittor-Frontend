2868aa0345911f40809da8d159e22fad8c7bd55b
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-04-26 14:02:34 -0700

    Se finalizó la funcionalidad del sistema de followers

--- src/components/User/BannerAvatar/BannerAvatar.jsx
+++ src/components/User/BannerAvatar/BannerAvatar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import ConfigModal from '../../Modal/ConfigModal'
 import EditUserForm from '../../User/EditUserForm'
 import AvatarNotFound from '../../../assets/png/avatar-no-found.png'
 import { API_HOST } from '../../../utils/constants'
+import { checkFollowApi, followUserApi, unfollowUserApi } from '../../../api/follow'
 
 import "./BannerAvatar.scss"
 
@@ -11,26 +12,61 @@ export default function BannerAvatar(props) {
     const { user, currentUser } = props;
 
     const [showModal, setShowModal] = useState(false);
+    const [following, setFollowing] = useState(null);
+    const [realodFollow, setRealodFollow] = useState(false)
 
     const bannerURL = user?.banner ? `${API_HOST}/obtenerBanner?id=${user.id}` : null;
     const avatarURL = user?.avatar ? `${API_HOST}/obtenerAvatar?id=${user.id}` : AvatarNotFound;
+
+    useEffect(() => { // Esto es para que no se ejecute en cada renderizado
+        if (user) {
+            checkFollowApi(user?.id).then(response => {
+                if (response?.status) {
+                    setFollowing(true);
+                } else {
+                    setFollowing(false);
+                }
+            });
+        }
+        setRealodFollow(false);
+    }, [user, realodFollow]);
+
+    const onFollow = () => {
+        followUserApi(user.id).then(() => {
+            setRealodFollow(true);
+        })
+    };
+
+    const onUnfollow = () => {
+        unfollowUserApi(user.id).then(() => {
+            setRealodFollow(true);
+        })
+    }
 
     return (
         <div className='banner-avatar' style={{ backgroundImage: `url('${bannerURL}')` }}>
             <div className='avatar' style={{ backgroundImage: `url('${avatarURL}')` }}></div>
             {user && (
                 <div className="options">
-                    {currentUser._id === user.id && 
+                    {currentUser._id === user.id &&
                         <Button onClick={() => setShowModal(true)}>Editar Perfil</Button>
                     }
-                    {currentUser._id !== user.id && (
-                        <Button>Seguir</Button>
-                     )}
+
+                    {currentUser._id !== user.id && 
+                    following !== null &&
+                    (following ? (
+                        <Button onClick={onUnfollow} className="unfollow">
+                            <span>Siguiendo</span>
+                        </Button>
+                    ) : (
+                        <Button onClick={onFollow}>Seguir</Button>
+                    ))}
+                    
                 </div>
             )}
-            <ConfigModal 
-                show={showModal} 
-                setShow={setShowModal} 
+            <ConfigModal
+                show={showModal}
+                setShow={setShowModal}
                 title="Editar perfil"
             >
                 <EditUserForm user={user} setShowModal={setShowModal} />

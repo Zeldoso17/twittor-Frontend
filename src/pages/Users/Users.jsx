01c4f3850ace76c15ea12df2a6f375ed44f1c77e
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-04-28 16:52:02 -0700

    Se finalizó la funcionalidad para mostrar a los usuarios que seguimos y a los usuarios que no, asi como entrar en sus perfiles

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -3,6 +3,7 @@ import { Spinner, ButtonGroup, Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import queryString from 'query-string'
 import { isEmpty } from 'lodash'
+import { useDebouncedCallback } from 'use-debounce';
 import BasicLayout from '../../layout/BasicLayout'
 import ListUsers from '../../components/ListUsers'
 import { withRouter } from '../../utils/functions'
@@ -18,18 +19,32 @@ function Users(props) {
     const [users, setUsers] = useState(null);
     const params = useUsersQuery(router);
     const [typeUser, setTypeUser] = useState(params.type || "follow");
+    const [btnLoading, setBtnLoading] = useState(false);
+
     const navigate = useNavigate();
 
-
-    console.log(props);
+    const onSearch = useDebouncedCallback((value) => {
+        setUsers(null);
+        navigate(`/users?page=${1}&type=${typeUser}&search=${value}`);
+    }, 200);
 
     useEffect(() => {
         getFollowsApi(queryString.stringify(params))
             .then(response => {
-                if (isEmpty(response)) {
-                    setUsers([]);
-                } else {
-                    setUsers(response);
+                // eslint-disable-next-line eqeqeq
+                if(params.page == 1) {
+                    if (isEmpty(response)) {
+                        setUsers([]);
+                    } else {
+                        setUsers(response);
+                    }
+                }else{
+                    if(!response){
+                        setBtnLoading(0);
+                    }else{
+                        setUsers([...users, ...response]);
+                        setBtnLoading(false);
+                    }
                 }
             })
             .catch(err => {
@@ -39,7 +54,6 @@ function Users(props) {
     }, [router.location])
 
     const onChangeType = type => {
-        //const history = MemoryRouter();
         setUsers(null);
         if (type === "new") {
             setTypeUser("new");
@@ -47,19 +61,25 @@ function Users(props) {
             setTypeUser("follow");
         }
         navigate(`/users?page=${1}&type=${type}&search=${""}`);
-       
+    }
+
+    const moreData = () => {
+        setBtnLoading(true);
+        const newPage = parseInt(params.page) + 1;
+        navigate(`/users?page=${newPage}&type=${typeUser}&search=${""}`);
     }
 
     return (
         <BasicLayout className="users" title="Usuarios" setRefreshCheckLogin={setRefreshCheckLogin}>
             <div className='users__title'>
                 <h2>Usuarios</h2>
-                <input type="texto" placeholder='Busca un usuario...' />
+                <input type="texto" placeholder='Busca un usuario...'
+                onChange={ e => onSearch(e.target.value)} />
             </div>
 
             <ButtonGroup className="users__options">
-                <Button onClick={() => onChangeType("follow")}>Siguiendo</Button>
-                <Button onClick={() => onChangeType("new")}>Nuevos</Button>
+                <Button className={typeUser === 'follow' && 'active'} onClick={() => onChangeType("follow")}>Siguiendo</Button>
+                <Button className={typeUser === 'new' && 'active'} onClick={() => onChangeType("new")}>Nuevos</Button>
             </ButtonGroup>
 
             {!users ? (
@@ -68,7 +88,16 @@ function Users(props) {
                     Buscando Usuarios
                 </div>
             ) : (
-                <ListUsers users={users} />
+                <>
+                    <ListUsers users={users} />
+                    <Button onClick={moreData} className="load-more">
+                        {!btnLoading ? (
+                            btnLoading !== 0 && "Cargar más usuarios"
+                        ) : (
+                            <Spinner as="span" animation='grow' seize="sm" role="status" aria-hidden="true"/>
+                        )}
+                    </Button>
+                </>
             )}
         </BasicLayout>
     );

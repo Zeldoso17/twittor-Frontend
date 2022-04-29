b6375687fa2c3763d53ccb72775aaeb646824e49
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-04-28 18:18:21 -0700

    Se finalizó la funcionalidad para poder ver los tweets de todos a los que sigo en el home y tambien se finalizó la pagina de Error404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import BasicLayout from "../../layout/BasicLayout";
+import { Link } from 'react-router-dom';
+import Error404Image from '../../assets/png/error-404.png';
+import Logo from '../../assets/png/logo.png'
 
 import "./Error404.scss";
 
 export default function Error404() {
   return (
-    <div>
-      <h2>Error404</h2>
+    <div className='error404'>
+      <img src={Logo} alt="Twittor" />
+      <img src={Error404Image} alt="Error 404" />
+      <Link to='/'>Volver al Inicio</Link>
     </div>
   )
 }
